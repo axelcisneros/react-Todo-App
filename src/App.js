2951ce4f4f87354027fa816b8ac8aacd1a0480ec
Axel Cisneros <axel.cisneros25@hotmail.com>
2023-03-23 01:21:46 -0600

    Todo-App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.scss';
+import Container from './components/Container';
 
 function App() {
   return (
     <div className="App">
-      
+      <Container />
     </div>
   );
 }
